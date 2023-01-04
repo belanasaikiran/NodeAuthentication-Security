6833b78e6e921a18ba2e0b69588ac90ece83ba2b
Saikiran Belana <88756154+belanasaikiran@users.noreply.github.com>
2023-01-04 14:57:33 +0530

    Level 3 - Hashing Passwords

--- app.js
+++ app.js
@@ -7,7 +7,12 @@ const ejs = require("ejs");
 const mongoose = require("mongoose")
 
 // Encryption
-const encrypt = require('mongoose-encryption')
+// const encrypt = require('mongoose-encryption')
+const md5 = require('md5') 
+
+// bcrypt - recommended for encryption of passwords
+// const bcrypt = require('bcrypt')
+// const saltRounds = 10
 
 const app = express();
 
@@ -32,7 +37,7 @@ const userSchema = new mongoose.Schema({
     password: String
 })
 
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields:  ['password']})
+// userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields:  ['password']})
 
 const User = new mongoose.model("User", userSchema)
 
@@ -62,7 +67,7 @@ app.get("/logout", (req, res)=> {
 app.post("/register", (req, res)=> {
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     })
     newUser.save((err)=>{
         if(err){
@@ -77,7 +82,7 @@ app.post("/register", (req, res)=> {
 // Login POST
 app.post("/login", (req,res)=> {
     const username = req.body.username
-    const password = req.body.password
+    const password = md5(req.body.password)
 
 
     User.findOne({email: username}, function(err, foundUser){
